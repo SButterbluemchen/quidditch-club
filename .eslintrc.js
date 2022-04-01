000e5fb5673ceb2a68854c0dd91177c6fbedce5a
SinaBrauch <sina.brauch@gmail.com>
2022-04-01 15:08:53 +0200

    first project initiation

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'jest/globals': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    'jest'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+};
