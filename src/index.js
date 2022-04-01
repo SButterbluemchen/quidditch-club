1a29435e96a3a6c69df2d8a4cfc210acd24e686a
SinaBrauch <sina.brauch@gmail.com>
2022-04-01 15:17:57 +0200

    sass boilerplate added and cleaned

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './stylesheets/main.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
