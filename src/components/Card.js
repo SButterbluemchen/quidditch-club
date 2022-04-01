da5ef40fab9b47d1ff32ea13370d4c1cc2396bd2
SinaBrauch <sina.brauch@gmail.com>
2022-04-01 18:24:37 +0200

    card branch with first static cards added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,21 @@
 import React from 'react';
 // import {useState, useEffet} from 'react';
 
-export default function Card() {
+export default function Card(props) {
+
+  function handlePlayerClick() {
+    console.log(`clicked on ${props.info.firstName}`);
+  }
 
   return (
-    <article className="card">
+    <article className="card" onClick={handlePlayerClick}>
       <figure>
-        <img src="/logo192.png" alt="Player" className="card-player--img"/>
+        <img src={props.info.image} alt="Player" className="card-player--img"/>
       </figure>
       <div className="card-player">
         <p className="card-player--number">1</p>
         <div className="card-player--information">
-          <p className="card-player--name">Harry Potter</p>
+          <p className="card-player--name">{props.info.name}</p>
           <p className="card-player--position">Attrapeur</p>
         </div>
       </div>
