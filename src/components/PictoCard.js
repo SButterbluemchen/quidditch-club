80f82ff25ff72946241993499337387b2a2ed27a
SinaBrauch <60073929+SButterbluemchen@users.noreply.github.com>
2022-04-06 12:33:21 +0200

    mini cards created and card effect added (#3)

---
+++ src/components/PictoCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function PictoCard(props) {
+
+  return (
+    <div>
+      <figure className="card-picto">
+        <img src={props.picto} />
+      </figure>
+      <p>{props.number}</p>
+      <p>{props.activity}</p>
+    </div>
+  );
+}
