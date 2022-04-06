55f3b472b80e6c75d06ffe813bb9d835477261db
Flavie Parmentier <flavie.parmentier@gmail.com>
2022-04-06 15:43:30 +0200

    created bullet vertical timeline with a single bludger icon

---
+++ src/components/BludgerIcon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BludgerIcon = () => {
+  const img = <img src="./bludger.png"></img>;
+  return (
+    <div>
+      {img}
+    </div>
+  );
+};
+
+export default BludgerIcon;
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component/dist-modules';
 import 'react-vertical-timeline-component/style.min.css';
+import BludgerIcon from './BludgerIcon';
 
 const Timeline = () => {
   const timelineElements = [
@@ -9,37 +10,33 @@ const Timeline = () => {
       title: 'History 1',
       description: 'lorem ipsum lorem ipsum',
       buttonText: 'blabla',
-      date: 'lorem 34',
-      icon: 'icon',
-
+      date: 'Février 2015',
+      icon: <BludgerIcon />,
     },
     {
       id: 2,
       title: 'History 2',
       description: 'lorem ipsum lorem ipsum',
       buttonText: 'blabla',
-      date: 'lorem 35',
-      icon: 'icon',
-
+      date: 'Octobre 2018',
+      icon: <BludgerIcon />,
     },
     {
       id: 3,
       title: 'History 2',
       description: 'lorem ipsum lorem ipsum',
       buttonText: 'blabla',
-      date: 'lorem 35',
-      icon: 'icon',
-
+      date: 'Mai 2020',
+      icon: <BludgerIcon />,
     },
     {
       id: 4,
-      title: 'History 2',
+      title: 'Avril 2022',
       description: 'lorem ipsum lorem ipsum',
       buttonText: 'blabla',
       date: 'lorem 35',
-      icon: 'icon',
-
-    }
+      icon: <BludgerIcon />,
+    },
   ];
 
   return (
@@ -53,7 +50,12 @@ const Timeline = () => {
                 key={timelineElement.key} //obligatoire lors d'un .map
                 date={timelineElement.date}
                 dateClassName="date"
+                icon={timelineElement.icon}
               >
+                <h3 className='vertical-timeline-element-title'>
+                  {timelineElement.title}
+                </h3>
+
 
               </VerticalTimelineElement>
             );
