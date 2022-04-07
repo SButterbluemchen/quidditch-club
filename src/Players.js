022129cdc84999c65c93c1f62e16c07b1bb0dab1
SinaBrauch <sina.brauch@gmail.com>
2022-04-07 21:22:43 +0200

    cards totally automated

--- src/Players.js
+++ src/Players.js
@@ -1,20 +1,68 @@
-import React from 'react';
 import Navbar from './components/Navbar';
 import CardSection from './components/CardSection';
+import React, {useEffect, useState} from 'react';
 
 export default function Players() {
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [playerGroups, setPlayerGroups] = useState([]);
+
+  const token = '7fb39549098e45b77abd3789b342e43e15d50e2e1681ccc2a5a349cc3f168f2791ee3a327a9db867d9248ca4225f3ef92eb9a2554894b692c6b9842ac555bce9b2c70924a752605a7f09876df23df3aadf46a97c6012d0eb994bbe8600f456dd40ac152ec3073eed375bba4a6fe38391c846591e2d2a01171dc3f86fc48a5cdf';
+ 
+  useEffect(() => {
+    const query = 'populate=%2A';
+    fetch(`http://localhost:1337/api/players?${query}`,
+      {
+        method: 'GET',
+        headers: {
+          'Accept': 'Application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      })
+      .then(response => response.json())
+      .then(data => { 
+        const allPlayerGroups = getPlayerGroups(data.data);
+
+        setPlayerGroups(allPlayerGroups);
+        setIsLoading(false);
+      });
+  }, []);
+
+  function getPlayerGroups(players) {
+    const groupedPlayers = groupBy(
+      players,
+      (player) => player.attributes.position,
+    );
+
+    const allPlayerGroups = [];
+
+    groupedPlayers.forEach((element, key) => {
+      allPlayerGroups.push({ position: key, players: element });
+    });
+    return allPlayerGroups;
+  }
+
+  function groupBy(list, keyGetter) {
+    const map = new Map();
+    list.forEach((item) => {
+      const key = keyGetter(item);
+      const collection = map.get(key);
+      if (!collection) {
+        map.set(key, [item]);
+      } else {
+        collection.push(item);
+      }
+    });
+    return map;
+  }
 
   return (
     <section className="page-players">
       <Navbar />
       <h2>Notre équipe de Quidditch</h2>
-      <CardSection position='Attrapeur'/>
-      <div className="page-players-div">
-        <CardSection position='Batteur'/>
-        <CardSection position='Poursuiveur'/>
-        <CardSection position='Gardien'/>
-        <CardSection position='Entraîneur'/>
-      </div>
+      {isLoading ? 'Loading...' : playerGroups.map(playerGroup => <CardSection key={playerGroup.position} position={playerGroup.position} players={playerGroup.players}/>)};
+      {/* <div className="page-players-div">
+      </div> */}
     </section>
   );
 }
--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -1,90 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import Card from './Card';
 
 const CardSection = (props) => {
-
-  const [isLoading, setIsLoading] = useState(true);
-  const [players, setPlayers] = useState(null);
-
-  const token = '7fb39549098e45b77abd3789b342e43e15d50e2e1681ccc2a5a349cc3f168f2791ee3a327a9db867d9248ca4225f3ef92eb9a2554894b692c6b9842ac555bce9b2c70924a752605a7f09876df23df3aadf46a97c6012d0eb994bbe8600f456dd40ac152ec3073eed375bba4a6fe38391c846591e2d2a01171dc3f86fc48a5cdf';
-
-  useEffect(() => {
-    const query = 'populate=%2A';
-    fetch(`http://localhost:1337/api/players?${query}`,
-      {
-        method: 'GET',
-        headers: {
-          'Accept': 'Application/json',
-          'Authorization': `Bearer ${token}`
-        }
-      })
-      .then(response => response.json())
-      .then(data => {
-        setPlayers(data.data);
-        setIsLoading(false);
-      });
-  }, []);
-
-  // const harryPotter = {
-  //   'data': [
-  //     {
-  //       'id': 1,
-  //       'attributes': {
-  //         'number': 1,
-  //         'firstName': 'Harry',
-  //         'lastName': 'Potter',
-  //         'position': 'Attrapeur',
-  //         'broom': 'Firebolt 5000',
-  //         'home': 'Gryffondor',
-  // "image": {
-  //   "data": {
-  //       "id": 1,
-  //       "attributes": {
-  //           "name": "harryPotter.jpeg",
-  //           "alternativeText": "harryPotter.jpeg",
-  //           "caption": "harryPotter.jpeg",
-  //           "width": 163,
-  //           "height": 227,
-  //           "formats": {
-  //               "thumbnail": {
-  //                   "name": "thumbnail_harryPotter.jpeg",
-  //                   "hash": "thumbnail_harry_Potter_eca7d93075",
-  //                   "ext": ".jpeg",
-  //                   "mime": "image/jpeg",
-  //                   "path": null,
-  //                   "width": 112,
-  //                   "height": 156,
-  //                   "size": 4.33,
-  //                   "url": "/uploads/thumbnail_harry_Potter_eca7d93075.jpeg"
-  //               }
-  //           },
-  //           "hash": "harry_Potter_eca7d93075",
-  //           "ext": ".jpeg",
-  //           "mime": "image/jpeg",
-  //           "size": 7.96,
-  //           "url": "/uploads/harry_Potter_eca7d93075.jpeg",
-  //           "previewUrl": null,
-  //           "provider": "local",
-  //           "provider_metadata": null,
-  //           "createdAt": "2022-04-06T14:42:46.871Z",
-  //           "updatedAt": "2022-04-06T14:42:46.871Z"
-  // }
-  //       }
-  //     }
-  //   ]
-  // };
-      
 
   return (
     <section className="card-section container">
       <h3>{props.position}</h3>
       <article className='card-section-players'>
-        {isLoading? 'Loading...' : players.map(player => {
-          if (player.attributes.position === props.position) {
-            return <Card key={player.id} infos={player.attributes} />;
-          }
-        }
-        )}
+        {props.players.map(player => <Card key={player.id} infos={player.attributes}/>)}
       </article>
     </section>
   );
