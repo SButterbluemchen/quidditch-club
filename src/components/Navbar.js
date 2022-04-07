95d3fab3b3f14f5099c59b76020a74756591b60c
SinaBrauch <60073929+SButterbluemchen@users.noreply.github.com>
2022-04-07 15:36:27 +0200

    Navbar and cards (#4)
    * mini cards created and card effect added
    
    * APIs connected to cards
    
    * No changes
    
    * navbar adapted to mobile and tablet
    
    * sass added to gitignore, hero image adapted to screen sizes
    
    * APIs connected to cards
    
    * No changes
    
    * navbar adapted to mobile and tablet
    
    * sass added to gitignore, hero image adapted to screen sizes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { Menubar } from 'primereact/menubar';
 
 export default function Navbar() {
+
+  const [windowWidth, setWindowWidth] = useState(()=> window.innerWidth);
+
+  useEffect(() => {
+    function watchWidth() {
+      setWindowWidth(window.innerWidth);
+    }
+    window.addEventListener('resize', watchWidth);
+    //cleanup function
+    return function() {
+      window.removeEventListener('resize', watchWidth);
+    };
+  }, []);
 
   const items = [
     {
@@ -19,13 +33,15 @@ export default function Navbar() {
   ];
 
   return (
-    <section className="navbar">
-      <article>
-        <figure>
-          <img src='logo-quidditch-white.png'/>
-        </figure>
-        <h3>Quidditch Club</h3>
-      </article>
+    <section className="navbar container">
+      <a href='/home'>
+        <article>
+          <figure>
+            <img src='logo-quidditch-white.png'/>
+          </figure>
+          {windowWidth >= 600 && <h3>Quidditch Club</h3>}
+        </article>
+      </a>
       <Menubar model={items}/>
     </section>
   );
