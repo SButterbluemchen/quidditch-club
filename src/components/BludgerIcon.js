3b7956a3b9a29f46f882d3471f9429454db3a777
Flavie <78551917+Trixkiddo@users.noreply.github.com>
2022-04-08 10:39:01 +0200

    let's pray (#8)

---
+++ src/components/BludgerIcon.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const BludgerIcon = () => {
+  const img = <img src="./bludger.png" alt="bludger"></img>;
+  return <div className="bludgericon-icon">{img}</div>;
+};
+
+export default BludgerIcon;
