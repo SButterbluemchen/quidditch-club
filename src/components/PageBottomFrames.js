70991b8b3bac00d40c7ad968dcbca9c1a2c1eb56
SinaBrauch <60073929+SButterbluemchen@users.noreply.github.com>
2022-04-08 15:03:39 +0200

    frame components to top and bottom added (#13)

---
+++ src/components/PageBottomFrames.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const pageBottomFrames = () => {
+  return (
+    <figure className="page-frames frame-bottom">
+      <img src="/corner-bottom-left.webp" alt="frame" />
+      <img src="/corner-bottom-right.webp" alt="frame" />
+    </figure>
+  );
+};
+
+export default pageBottomFrames;
---
+++ src/components/pageTopFrames.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const pageTopFrames = () => {
+  return (
+    <figure className="page-frames frame-top">
+      <img src="/corner-left.webp" alt="frame" />
+      <img src="/corner-right.webp" alt="frame" />
+    </figure>
+  );
+};
+
+export default pageTopFrames;
