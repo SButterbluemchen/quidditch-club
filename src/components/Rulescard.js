6ed3a2bc442316dcad4c2827b544c93b5c21acb1
Flavie <78551917+Trixkiddo@users.noreply.github.com>
2022-04-13 15:39:12 +0200

    Update Rulescard.js

--- src/components/Rulescard.js
+++ src/components/Rulescard.js
@@ -4,10 +4,10 @@ import React from 'react';
 /* Composant Rulescard : dessin + un peu de description */
 const Rulescard = (props) => {
   return (
-    <div className='rulescard-card'>
-      <div className='rulescard-body'>
-        <img className='rulescard-img' src={props.img}/>
-        <p className='rulescard-description'>{props.description}</p>
+    <div className="rulescard-card">
+      <div className="rulescard-body">
+        <img className="rulescard-img" src={props.img}/>
+        <p className="rulescard-description">{props.description}</p>
 
       </div>
 
