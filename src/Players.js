03b642fb8ce352b8096b80221e880c0028b1ccf8
SinaBrauch <sina.brauch@gmail.com>
2022-04-19 22:19:34 +0200

    searchbar code fixed

--- src/Players.js
+++ src/Players.js
@@ -10,6 +10,8 @@ export default function Players() {
   const [playerGroups, setPlayerGroups] = useState([]);
   const [allPlayers, setAllPlayers] = useState([]);
 
+  const [query, setQuery] = useState('');
+
   const token = '7fb39549098e45b77abd3789b342e43e15d50e2e1681ccc2a5a349cc3f168f2791ee3a327a9db867d9248ca4225f3ef92eb9a2554894b692c6b9842ac555bce9b2c70924a752605a7f09876df23df3aadf46a97c6012d0eb994bbe8600f456dd40ac152ec3073eed375bba4a6fe38391c846591e2d2a01171dc3f86fc48a5cdf';
  
   useEffect(() => {
@@ -25,11 +27,27 @@ export default function Players() {
       .then(response => response.json())
       .then(data => { 
         setAllPlayers(data.data);
-        const allPlayerGroups = getPlayerGroups(data.data);
-        setPlayerGroups(allPlayerGroups);
         setIsLoading(false);
       });
   }, []);
+
+  useEffect(() => {
+    setAllPlayersGroups();
+  }, [allPlayers, query]);
+
+  function setAllPlayersGroups() {
+    const filteredPlayers = query.length > 0 ? allPlayers.filter(player => {
+      if (query !== '' && player.attributes.firstName.toLowerCase().includes(query.toLowerCase())) { 
+        return player;
+      }
+    }
+    ) : allPlayers;
+
+    const allPlayerGroups = getPlayerGroups(filteredPlayers);
+
+    setPlayerGroups(allPlayerGroups);
+  }
+
   // Get data from API and group OBJECTS in 1 OBJECT by position - using 'groupBy' function from below
   function getPlayerGroups(players) {
     const groupedPlayers = groupBy(
@@ -84,9 +102,6 @@ export default function Players() {
     return allPlayerGroups;
   }
 
-  // SEARCHBAR
-  const [query, setQuery] = useState('');
-
   return (
     <section className="page-players">
       <Navbar />
@@ -98,24 +113,6 @@ export default function Players() {
         </div>
       </section>
       {isLoading ? 'Loading...' : playerGroups.map(playerGroup => <CardSection key={playerGroup.position} position={playerGroup.position} players={playerGroup.players}/>)}
-      {allPlayers.filter(player => {
-        if (query !== '' && player.attributes.firstName.toLowerCase().includes(query.toLowerCase())) { 
-          return player;
-        }
-      }
-      ).map(player => 
-        <li key={player.id}>{player.attributes.firstName}</li>
-      )}
-      {/* {isLoading ? 'Loading...' : allPlayers.filter(player => {
-        if (query === '') {
-          return player;
-        }
-        else if (query !== '' && player.attributes.firstName.toLowerCase().includes(query.toLowerCase())) { 
-          return player;
-        }
-      }).map(player => 
-        playerGroups.map(playerGroup => <CardSection key={playerGroup.position} position={playerGroup.position} players={playerGroup.players}/>)
-      )} */}
       <PageBottomFrames />
     </section>
   );
