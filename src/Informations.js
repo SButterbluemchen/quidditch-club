f87a0ee0f38064ea8b2830ffd4a9f9c2298dd187
SinaBrauch <60073929+SButterbluemchen@users.noreply.github.com>
2022-04-22 16:08:03 +0200

    Snitch (#44)
    * searchbar first commit
    
    * first searchbar push
    
    * searchbar first integration into players.js
    
    * new searchcode added
    
    * searchbar code fixed
    
    * player searchbar filter finished, sorting and other conditions added to searchbar
    
    * first searchbar push
    
    * searchbar first commit
    
    * first searchbar push
    
    * contact page created
    
    * snitch added to home, informations, contact pages

--- src/Informations.js
+++ src/Informations.js
@@ -6,6 +6,7 @@ import Trainingplan from './components/Trainingsplan/Trainingplan';
 import ImageTextBlock from './components/ImageTextBlock';
 import RankingTable from './components/Ranking/RankingTable';
 import InscriptionForm from './components/InscriptionForm';
+import ArrowToBeginning from './components/ArrowToBeginning';
 
 const Informations = () => {
   return (
@@ -13,9 +14,12 @@ const Informations = () => {
       <Navbar />
       <PageTopFrames />
       <Trainingplan />
+      <ArrowToBeginning />
       <RankingTable />
+      <ArrowToBeginning />
       <ImageTextBlock sectionTitle="Bienvenu dans notre stade" image="/stadium2.jpeg" title="Un stade au coeur de Poudlard" />
       <ImageTextBlock articleClassName="right" image="/stadium.jpeg" title="Une pelouse de qualité pour plus de succès"/>
+      <ArrowToBeginning />
       <InscriptionForm />
       <PageBottomFrames />
     </section>
